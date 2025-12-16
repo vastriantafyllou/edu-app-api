79612547bb38651c819f1d8cc1d5c3c70b048f60
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-12-17 00:08:25 +0200

    feat(core): add domain enums

---
+++ src/main/java/gr/aueb/cf/eduapp/core/enums/GenderType.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.eduapp.core.enums;
+
+public enum GenderType {
+    MALE, FEMALE, OTHER
+}
---
+++ src/main/java/gr/aueb/cf/eduapp/core/enums/Role.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.eduapp.core.enums;
+
+public enum Role {
+    TEACHER,
+    EMPLOYEE,
+    SUPER_ADMIN
+}
